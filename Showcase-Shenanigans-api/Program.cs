3d50ff35527ff45a9c3b99b4796532549df51e1f
ian speelman <ianspeelman@gmail.com>
2024-09-10 08:24:22 +0200

    Add base backend and database

--- Showcase-Shenanigans-api/Program.cs
+++ Showcase-Shenanigans-api/Program.cs
@@ -1,6 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Showcase_Shenanigans_api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+  options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -12,8 +23,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
